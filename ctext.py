133484ef822cb57ed00fc4b3650b528ed85c2d0f
Svetlana <SvetlanaM@users.noreply.github.com>
2016-09-04 08:58:30 +0200

    Update ctext.py

--- ctext.py
+++ ctext.py
@@ -6,19 +6,19 @@ import tinys3
 import time
 import json
 
-def convert(list_of_indexes):
+def convert(list_of_indexes, sheet_names):
     list_of_indexes = list_of_indexes
     listOfFiles = glob.glob("/data/in/tables/*.csv")
-
+    sheet_names = sheet_names
+    
+    excelFile = xlsxwriter.Workbook('sir' + '.xlsx')
     for index, fileInList in enumerate(listOfFiles):
-        fileName  = fileInList[0:fileInList.find('.')]
-        excelFile = xlsxwriter.Workbook(fileName + '.xlsx')
-        worksheet = excelFile.add_worksheet()
+        worksheet = excelFile.add_worksheet(str(sheet_name[index]))
         with open(fileInList, 'rb') as f:
             content = csv.reader(f)
             for index_row, data_in_row in enumerate(content):
                 for index_col, data_in_cell in enumerate(data_in_row):
-                    if index_col  in list_of_indexes:
+                    if index_col  in list_of_indexes[index]:
                         if type(data_in_cell) == int or type(data_in_cell) == float:
                             temp = '=TEXT(%d,\"*#*,######\")' % (data_in_cell)
                             worksheet.write_formula(index_row, index_col, temp)
@@ -35,5 +35,5 @@ def convert(list_of_indexes):
                             worksheet.write_number(index_row, index_col, data_in_cell)
                         else:
                             worksheet.write(index_row, index_col, unicode(data_in_cell))
+    print " === conversion is done ==="
     excelFile.close()
-    print " === conversion  is done ==="
